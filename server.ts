fca582a2190e72c62f445cccf23d925ce21e2d41
Krzysiek Szymczak <rakaz666@gmail.com>
2016-01-09 19:04:17 +0100

    typescripted v0.2

---
+++ server.ts
@@ -0,0 +1,12 @@
+import _ = require('lodash');
+import express = require('express');
+
+var app = express();
+
+app.get("/", function(req, res){
+    res.send("Hello World BIJACZ!");
+});
+
+app.listen(process.env.PORT || 1337, function(){
+    console.log("app started ad 1337");
+});
